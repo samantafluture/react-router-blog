b1dcbe5fcabe6c9448a039d95b9fcb73541f9130
Samanta Fluture <samantafluture@gmail.com>
2021-07-10 23:52:21 -0300

    use react router for nested nav

---
+++ petshop/src/components/Cabecalho.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import imagem from "../assets/img/doguito.svg";
+import '../assets/css/componentes/cabecalho.css';
+
+const Cabecalho = () => {
+  return (
+    <header className="cabecalho container">
+      <div className="menu-hamburguer">
+        <span className="menu-hamburguer__icone"></span>
+      </div>
+      <div className="cabecalho-container">
+        <a href="/" className="flex flex--centro">
+          <img className="cabecalho__logo" src={imagem} alt="logo doguito" />
+          <h1 className="cabecalho__titulo">PetShop</h1>
+        </a>
+      </div>
+      <nav className="menu-cabecalho">
+        <ul className="menu-itens">
+          <li>
+            <Link to="/" className="menu-item menu-item--entrar">
+              Entrar
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="menu-item">
+              Produtos
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="menu-item">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link to="/sobre" className="menu-item">
+              Sobre
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="menu-cabecalho-background"></div>
+    </header>
+  );
+};
+
+export default Cabecalho;
